afa5d9ddb162773438e7495e99076c466b3c5f40
Sundaram Saroj <sundaramsaroj@Sundarams-MacBook-Air.local>
2025-05-18 14:51:58 +0530

    setup supabase fetching

--- backend/my-app/scheduler.js
+++ backend/my-app/scheduler.js
@@ -1,13 +1,26 @@
 import nc from "node-cron";
-// function logMessage(){
-//     console.log("This si a cron job executed by node-cron in nodejs")
-// }
-
+import dotenv from "dotenv";
+dotenv.config();
+import { createClient  } from "@supabase/supabase-js";
+const db_url=process.env.DATABASE_URL
+const supabase=createClient(
+    process.env.SUPABASE_URL,
+    process.env.ANON_KEY
+)
+const scheduled_date=fetchDate()
+const {data,error}=await supabase .from('Details').select('yourEmail').eq('date',scheduled_date)
+if(error){
+    console.log("Error fetching the DB",error)
+}
+else{
+    // const email=data[0].email
+    console.log("Successfully fetched the data",data)
+}
 function fetchDate(){
      const date = new Date();
     const day = String(date.getDate()).padStart(2, '0');     
     const month = String(date.getMonth() + 1).padStart(2, '0'); 
     const year = date.getFullYear(); 
-    const formattedDate=console.log(`${year}/${month}/${day}`);
+    return (`${year}-${month}-${day}`);
 }
-fetchDate()
+
